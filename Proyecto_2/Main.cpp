c266f3846de1cf706ebb807acb2125245162cc82
Alfredo Mercado Rios <amercado.rios1905@gmail.com>
2023-11-05 01:12:25 -0600

    Main merged

--- Proyecto_2/Main.cpp
+++ Proyecto_2/Main.cpp
@@ -7,40 +7,74 @@
 #include <cstdlib>
 
 int main(){
-/*     StructMundo * m1 = new StructMundo();
+    StructMundo * m1 = new StructMundo();
     m1->generarPoblacion(300);
 
-    m1->imprimir();
-    m1->buscarPorReligion("Cristianismo");
+    //m1->imprimir();
     m1->buscarPorReligion("Cristianismo");
     m1->buscarPorReligion("Islam");
     m1->buscarPorReligion("Judaismo");
+    m1->buscarPorReligion("Hinduismo");
+    m1->buscarPorReligion("Budismo");
+    m1->buscarPorReligion("Sikhismo");
+    m1->buscarPorReligion("Jainismo");
+    m1->buscarPorReligion("Zoroastrismo");
+    m1->buscarPorReligion("Bahai");
+    m1->buscarPorReligion("Shintoismo");
+    m1->buscarPorReligion("Cristianismo");
+    m1->buscarPorReligion("Islam");
     m1->buscarPorReligion("Judaismo");
     m1->buscarPorReligion("Hinduismo");
+    m1->buscarPorReligion("Budismo");
+    m1->buscarPorReligion("Sikhismo");
+    m1->buscarPorReligion("Jainismo");
+    m1->buscarPorReligion("Zoroastrismo");
     m1->buscarPorReligion("Bahai");
+    m1->buscarPorReligion("Shintoismo");
+    m1->buscarPorReligion("Cristianismo");
+    m1->buscarPorReligion("Islam");
+    m1->buscarPorReligion("Judaismo");
+    m1->buscarPorReligion("Hinduismo");
     m1->buscarPorReligion("Budismo");
+    m1->buscarPorReligion("Sikhismo");
+    m1->buscarPorReligion("Jainismo");
+    m1->buscarPorReligion("Zoroastrismo");
+    m1->buscarPorReligion("Bahai");
+    m1->buscarPorReligion("Shintoismo");
+    m1->buscarPorReligion("Cristianismo");
+    m1->buscarPorReligion("Islam");
+    m1->buscarPorReligion("Judaismo");
+    m1->buscarPorReligion("Hinduismo");
     m1->buscarPorReligion("Budismo");
-    
+    m1->buscarPorReligion("Sikhismo");
+    m1->buscarPorReligion("Jainismo");
+    m1->buscarPorReligion("Zoroastrismo");
+    m1->buscarPorReligion("Bahai");
+    m1->buscarPorReligion("Shintoismo");
     //m1->imprimir();
-    //cout << m1->poblacion[1]->cantPecados() << endl;
-    cout << "-----------------------TESTING-------------" << endl;
-    m1->demonios[1]->condenar((m1->poblacion), m1->cantPoblacion);
-    //cout << m1->demonios[1]->familias[0]->personas->index << endl;
-    m1->demonios[1]->imprimirFamilias();
 
-    cout << "TESTEANDO LA VARA"  << endl;
-    StructHumano * h1 = m1->quitarMasPecador();
-    if (h1 != nullptr)
-        h1->imprimir(); */
-
-    StructMundo * m1 = new StructMundo();
-    m1->generarPoblacion(1000);
-
-    m1->arbol->inOrder(m1->arbol->raiz);
-
-    cout << "-----------------------TESTING-------------" << endl;
-    m1->arbolTernarioAngeles->llenarNivel();
+    for (int i = 0; i < 7; i++){
+        m1->demonios[i]->condenar(m1->poblacion,m1->cantPoblacion);
+    }
+    
+    m1->salvacion();
     m1->arbolTernarioAngeles->imprimir(m1->arbolTernarioAngeles->raiz);
+        
+    
+    /*
+    StructMundo * m1 = new StructMundo();
+    StructHumano * h1 = new StructHumano(1,"beto","alvarez","cr","cristo","code","ya");
+    StructHumano * h3 = new StructHumano(2,"alfredo","alvarez","cr","cristo","code","ya");
+    ArbolTernario * t1 = new ArbolTernario();
+    t1->imprimir(t1->raiz);
+    t1->llenarNivel();
+    cout << "\n\nLLENAR NIVEL que es esto: " << endl << endl;
+    
+    t1->insertarHumano(h1,t1->raiz);
+    t1->insertarHumano(h3,t1->raiz);
+    t1->imprimir(t1->raiz);
+    cout << "test index arbol: " << pow(3,t1->nivel-1) << endl;
+    */
     
     cout << "-----------------------TESTING2-------------" << endl;
     //Envio de correos electronicos template
